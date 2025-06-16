35b0c883e5e2c2cdf4b98e85fe8bdeb81f3a03e4
marceloacosta <acosta.cavalero@gmail.com>
2025-06-16 16:08:14 -0400

    chore: Bump version to 0.18.0

--- aws_super_cli/__init__.py
+++ aws_super_cli/__init__.py
@@ -1,5 +1,5 @@
 """AWS Super CLI - Multi-account AWS resource discovery with service-level cost intelligence."""
 
-__version__ = "0.17.0"
+__version__ = "0.18.0"
 __author__ = "Marcelo Acosta"
 __description__ = "See your AWS resources across all accounts in one command" 
